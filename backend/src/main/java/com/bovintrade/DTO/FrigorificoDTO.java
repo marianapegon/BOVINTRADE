cc079f7aba214d6a5b571635596d421e0d1ca5f1
Elisandra Carol da Silva <166402321+elicarolslv@users.noreply.github.com>
2025-06-16 02:37:50 -0300

    Add files via upload

---
+++ backend/src/main/java/com/bovintrade/DTO/FrigorificoDTO.java
@@ -0,0 +1,41 @@
+package com.bovintrade.DTO;
+
+public class FrigorificoDTO {
+    private String nome;
+    private String cnpj;
+    private String email;
+    private String telefone;
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
---
+++ backend/src/main/java/com/bovintrade/repository/TransporteRepository.java
@@ -0,0 +1,7 @@
+package com.bovintrade.repository;
+
+import com.bovintrade.model.Transporte;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransporteRepository extends JpaRepository<Transporte, Integer> {
+}
