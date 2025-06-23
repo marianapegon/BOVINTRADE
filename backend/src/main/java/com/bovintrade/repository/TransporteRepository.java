de281a168802a9e86b51605e2f3aa64e105cb0ac
Elisandra Carol da Silva <166402321+elicarolslv@users.noreply.github.com>
2025-06-23 15:20:32 -0300

    Delete backend/src directory

--- backend/src/main/java/com/bovintrade/repository/TransporteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bovintrade.repository;
-
-import com.bovintrade.model.Transporte;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransporteRepository extends JpaRepository<Transporte, Integer> {
-}
--- backend/src/main/java/com/bovintrade/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bovintrade.repository;
-
-import com.bovintrade.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    Usuario findByEmailAndSenha(String email, String senha);
-}
